190db23b7f1883a8754395d7d4086db1313b4427
JEEVIKAGUPTA24 <106991415+JEEVIKAGUPTA24@users.noreply.github.com>
2022-06-19 18:22:30 +0530

    Add files via upload

---
+++ AREA OF A CIRCLE.py
@@ -0,0 +1,9 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from math import pi
+>>> r = float(input("Enter radius of circle :"))
+Enter radius of circle :1.1
+>>> print ("Area of the circle is: " + str(pi*r**2))
+Area of the circle is: 3.8013271108436504
+>>> 
