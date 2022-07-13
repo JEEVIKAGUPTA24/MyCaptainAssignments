0b83ed80509f9ba63614a2f8349df58ebe988e50
JEEVIKAGUPTA24 <106991415+JEEVIKAGUPTA24@users.noreply.github.com>
2022-07-13 18:12:56 +0530

    Delete AREA OF A CIRCLE.py

--- AREA OF A CIRCLE.py
+++
@@ -1,8 +0,0 @@
-Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
-
->>> from math import pi
->>> r = float(input("Enter radius of circle :"))
->>> print ("Area of the circle is: " + str(pi*r**2))
-
