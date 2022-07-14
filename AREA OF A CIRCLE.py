f2deffc6eb3b56e84180788b0eb85a86904099f6
JEEVIKAGUPTA24 <106991415+JEEVIKAGUPTA24@users.noreply.github.com>
2022-07-14 10:10:15 +0530

    Add files via upload

---
+++ AREA OF A CIRCLE.py
@@ -0,0 +1,10 @@
+import math
+radius = float(input("Enter the radius of the circle :"))
+area = math.pi * radius * radius
+print("Area of the circle is :{0}".format(area))
+
+>>>
+fileName  = input(" Input the Filename : " )
+file_extns = filename.spilt("'")
+print("The extension of the file is :"+ repr(file_extns[-1]))
+
